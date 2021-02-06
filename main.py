8fb7aa01b73cbdb3d684faedc74d4a233ce6ad6f
hebowei2000 <2312217486@qq.com>
2021-02-06 21:52:51 +0800

    main

--- main.py
+++ main.py
@@ -15,6 +15,20 @@ from marvil_policy import marvil_policy
 from bnn import EnsembledBNN
 from fakenv import FakeEnv
 
+class StaticFns:
+    @staticmethod
+    def termination_fn(obs, act, next_obs):
+        assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
+        done = np.array([False]).repeate(len(obs))
+        done = done[:,None]
+        return done
+
+    @staticmethod
+    def recompute_reward_fn(obs, act, next_obs, rew):
+        assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
+        new_rew = -(rew + 0.1 * np.sum(np.square)(act))) - 0.1 * np.sum(np.square(act))
+        return new_rew
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--task", type=str, default="CartPole-v0")
@@ -23,3 +37,8 @@ def get_args():
     parser.add_argument("--batch-size", type=int, default=2000)
     parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu")
     return parser.parse_args()
+
+def main(args=get_args()):
+    envs = gym.make(args.task)
+    args.state_shape = env.observation_space.shape or env.observation_space.n
+    
